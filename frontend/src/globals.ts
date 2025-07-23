d44dcf72f341ad627b6e70988515a2527729e63b
thesagarmahajan <sagarmahajan31997@gmail.com>
2025-07-23 11:31:41 +0530

    AWS URL Changed

--- frontend/src/globals.ts
+++ frontend/src/globals.ts
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:8888"
+export const baseUrl = "http://3.110.46.164:8888"
 export const categoriesUrl = `${baseUrl}/categories`
 export const expensesUrl = `${baseUrl}/expenses`
 export const authUrl = `${baseUrl}/users`
